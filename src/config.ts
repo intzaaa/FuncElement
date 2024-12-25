11be44b61eb04d971a958e8b1aa624616a1ad471
intza <huangyinhaow@outlook.com>
2024-12-25 22:14:46 +0800

    support partial config

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
-export const default_config = {
+const default_config = {
   document: globalThis?.document,
 } as const;
 
 export let config = default_config;
 
-export const set_config = (_config: typeof default_config) => {
-  config = _config;
+export const set_config = (_config: Partial<typeof default_config>) => {
+  return Object.assign(config, _config);
 };
